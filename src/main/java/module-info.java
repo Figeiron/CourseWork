3c97d5008ec0f03488f03c86386d3ee93c7cbb30
Figeiron <51824547+figeiron@users.noreply.github.com>
2024-11-15 20:42:35 +0200

    initial

---
+++ src/main/java/module-info.java
@@ -0,0 +1,17 @@
+module org.server.coursework {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+    requires kotlin.stdlib;
+
+    opens org.server.coursework to javafx.fxml;
+    exports org.server.coursework;
+}
