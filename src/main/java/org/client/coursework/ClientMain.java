bd18cde730019cdce286c234dbe3285e2919be32
Figeiron <51824547+figeiron@users.noreply.github.com>
2024-11-15 21:50:34 +0200

    added main class for client app

---
+++ src/main/java/org/client/coursework/ClientMain.java
@@ -0,0 +1,16 @@
+package org.client.coursework;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class ClientMain extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
