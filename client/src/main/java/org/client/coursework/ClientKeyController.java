4569d56a21471fe39083332cbc34fd6bb7ad96aa
Figeiron <51824547+figeiron@users.noreply.github.com>
2024-11-16 12:45:09 +0200

    added key controller

---
+++ client/src/main/java/org/client/coursework/ClientKeyController.java
@@ -0,0 +1,21 @@
+package org.client.coursework;
+
+import javafx.scene.input.KeyEvent;
+import javafx.event.EventHandler;
+import javafx.stage.Stage;
+
+public class ClientKeyController implements EventHandler<KeyEvent> {
+
+    private final Stage stage;
+
+    public ClientKeyController(Stage stage) {
+        this.stage = stage;
+    }
+
+    @Override
+    public void handle(KeyEvent event) {
+        if (event.getCode().toString().equals("F11")) {
+            stage.setFullScreen(!stage.isFullScreen());
+        }
+    }
+}
--- client/src/main/java/org/client/coursework/ClientMain.java
+++ client/src/main/java/org/client/coursework/ClientMain.java
@@ -11,7 +11,11 @@ public class ClientMain extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(ClientMain.class.getResource("client_view.fxml"));
+        ClientKeyController ClientKeyController = new ClientKeyController(stage);
+
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        scene.setOnKeyPressed(ClientKeyController);
+
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
